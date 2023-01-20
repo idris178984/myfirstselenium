7bc505763ec71dea491ea1a41c5e41500fc9cdbe
IdrisAkca <eylul178984@gmail.com>
2023-01-20 13:39:31 +0300

    DAY8 Practice Task4

---
+++ src/test/java/com/cydeo/tests/DAY8_Properties_config_reader/Practice/Task4.java
@@ -0,0 +1,31 @@
+package com.cydeo.tests.DAY8_Properties_config_reader.Practice;
+
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+
+       // TC #4: Google search
+       // 1- Open a chrome browser
+        // 2- Go to: https://google.com
+        Driver.getDriver().get("https://www.google.com");
+
+
+       // 3- Write “apple” in search box
+        WebElement searchBox=Driver.getDriver().findElement(By.xpath("//input[@name='q']"));
+        searchBox.sendKeys(ConfigurationReader.getProperty("searchValue")+ Keys.ENTER);
+
+        //4- Verify title:
+       // Expected: apple - Google Search
+        String expectedTitle=ConfigurationReader.getProperty("searchValue")+"-Google Ara";
+        String actualTitle=Driver.getDriver().getTitle();
+
+
+
+    }
+}
