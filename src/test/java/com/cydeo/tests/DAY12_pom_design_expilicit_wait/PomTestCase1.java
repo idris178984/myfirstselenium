68e2838a662fccfea87eca0f20041c71ddd0cf81
IdrisAkca <eylul178984@gmail.com>
2023-02-13 17:52:46 +0300

    DAY12

---
+++ src/test/java/com/cydeo/tests/DAY12_pom_design_expilicit_wait/PomTestCase1.java
@@ -0,0 +1,35 @@
+package com.cydeo.tests.DAY12_pom_design_expilicit_wait;
+
+import com.cydeo.pages.TestCase1;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PomTestCase1 {
+
+    TestCase1 testCase1;
+    @BeforeMethod
+    public void setUpMethod(){
+        Driver.getDriver().get(ConfigurationReader.getProperty("link"));
+        testCase1=new TestCase1();
+
+    }
+    @Test
+    public void TestCase1_test(){
+        testCase1.homePage.isDisplayed();
+        testCase1.signUp.click();
+        testCase1.newUserSignUp.isDisplayed();
+
+        Faker faker=new Faker();
+        Actions actions=new Actions(Driver.getDriver());
+
+        testCase1.enterName.sendKeys(faker.name().firstName());
+
+
+
+    }
+
+}
